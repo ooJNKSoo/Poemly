30ad2195a2c638fa2f50c16d41db7674ef9a05ef
ooJNKSoo <57141065+ooJNKSoo@users.noreply.github.com>
2020-08-28 14:31:02 +0100

    Update client.py

--- client.py
+++ client.py
@@ -13,9 +13,27 @@ canSend = 0
 
 clientSocket.connect((hostIp, portNumber))
 
+window = Tk()
+window.title("poemly")
+
+fullPoem = Text(window, width=50)
+fullPoem.grid(row=0, column=0, padx=10, pady=10)
+
+yourPoemLine = Entry(window, width=50)
+yourPoemLine.insert(0,"")
+yourPoemLine.grid(row=1, column=0, padx=10, pady=10)
+
 
 def sendLine():
 	#sends a line to the server
+	poemLine = yourPoemLine.get()
+
+        yourPoemLine.delete("0",END)
+        clientSocket.send(poemLine.encode("utf-8"))
+
+
+sendLineBtn = Button(window, text="Add to Poem", width=20, command=sendLine)
+sendLineBtn.grid(row=2, column=0, padx=10, pady=10)
 
 
 def recvLine():
