c9e8c51753b3eea9f9d07cb3438fea3f45cc1675
ooJNKSoo <57141065+ooJNKSoo@users.noreply.github.com>
2020-08-29 13:59:20 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,32 @@
 from socket import *
-from tkinter import *
 from threading import *
+import random
 
 clients = []
+players = 4
 
-def clientThread()
-	#takes care of most things
-	 while True:
+
+def clientThread(clientSocket, clientAddress):
+    i = 0
+    while True:
+        clients[i].send("0110011101101111".encode("utf-8"))
         line = clients[i].recv(1024).decode("utf-8")
         print(clientAddress[0] + ":" + str(clientAddress[1]) + " says: " + line)
+        i = i+1
 
         for client in clients:
             client.send(("added:     " + line).encode("utf-8"))
 
-        if not line:
-        clients.remove(clientSocket)
-        print(clientAddress[0] + ":" + str(clientAddress[1]) + " disconnected")
-        break
-    clientSocket.close()
+        if i > (players-1):
+            for client in clients:
+                client.send(" game over".encode("utf-8"))
+            break
 
+        if not line:
+            clients.remove(clientSocket)
+            print(clientAddress[0] + ":" + str(clientAddress[1]) + " disconnected")
+            break
+    clientSocket.close()
 
 
 hostSocket = socket(AF_INET, SOCK_STREAM)
@@ -28,13 +36,14 @@ hostIp = "127.0.0.1"
 portNumber = 7500
 hostSocket.bind((hostIp, portNumber))
 hostSocket.listen()
-hostSocket.listen()
 print ("Waiting for connection...")
 
 
 while True:
     clientSocket, clientAddress = hostSocket.accept()
     clients.append(clientSocket)
-    print ("Connection established with: ", clientAddress[0] + ":" + str(clientAddress[1]))   
-    thread = Thread(target=clientThread, args=(clientSocket, clientAddress, ))
-    thread.start()
+    print ("Connection established with: ", clientAddress[0] + ":" + str(clientAddress[1]))
+    if len(clients) > (players-1):
+        random.shuffle(clients)
+        thread = Thread(target=clientThread, args=(clientSocket, clientAddress, ))
+        thread.start()
